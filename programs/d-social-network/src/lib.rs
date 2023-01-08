ea0217e978a7de2b9d76f816d734bb4b76834a34
kubo12312 <rosina.jakub@gmail.com>
2023-01-08 14:29:17 +0100

    First instruction and test, add prettier and eslint

--- programs/d-social-network/src/lib.rs
+++ programs/d-social-network/src/lib.rs
@@ -6,10 +6,52 @@ declare_id!("5dgW78UWc7hT4jxgh4XYr4NN6rcqDUndg7JzzUeGCskL");
 pub mod d_social_network {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn post_send(ctx: Context<SendPost>, content: String) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        let creator: &Signer = &ctx.accounts.creator;
+        let time: Clock = Clock::get().unwrap();
+
+        if content.chars().count() > 320 {
+            return Err(ErrorCode::ContentIsLong.into())
+        }
+
+        post.creator = *creator.key;
+        post.timestamp = time.unix_timestamp;
+        post.content = content;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendPost<'info> {
+    #[account(init, payer = creator, space = Post::LEN)]
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct Post {
+    pub creator: Pubkey,
+    pub timestamp: i64,
+    pub content: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBKEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const CONTENT_LENGTH: usize = 320 * 4;
+
+impl Post {
+    const LEN: usize = DISCRIMINATOR_LENGTH + PUBKEY_LENGTH + TIMESTAMP_LENGTH + STRING_LENGTH_PREFIX + CONTENT_LENGTH;
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Content has max length 320 characters.")]
+    ContentIsLong,
+}
--- tests/d-social-network.ts
+++ tests/d-social-network.ts
@@ -1,16 +1,28 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { DSocialNetwork } from "../target/types/d_social_network";
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { DSocialNetwork } from '../target/types/d_social_network'
+import * as assert from 'assert'
 
-describe("d-social-network", () => {
+describe('d-social-network', () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.DSocialNetwork as Program<DSocialNetwork>;
+  const program = anchor.workspace.DSocialNetwork as Program<DSocialNetwork>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  it('send post', async () => {
+    const post = anchor.web3.Keypair.generate()
+    await program.rpc.postSend('First Content', {
+      accounts: {
+        post: post.publicKey,
+        creator: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [post],
+    })
+    const postAccount = await program.account.post.fetch(post.publicKey)
+
+    assert.equal(postAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58())
+    assert.equal(postAccount.content, 'First Content')
+    assert.ok(postAccount.timestamp)
+  })
+})
